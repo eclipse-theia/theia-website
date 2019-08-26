323da15c61c1bed089843c80abb951b563086336
nisarhassan12 <syednisarhassan12@gmail.com>
2019-08-26 11:39:41 +0000

    Adds Missing Images to the Docs.
    Signed-off-by: nisarhassan12 <syednisarhassan12@gmail.com>

---
+++ src/components/DocImage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { breakpoints } from '../utils/variables'
+
+const StyledDocImage = styled.div`
+    .docimage {
+        width: ${({width}) => width ? width : '100%'};
+        min-width: 30rem;
+        margin: 2rem 0;
+        object-fit: contain;
+        &-container {
+            max-width: 114.5rem;
+            margin: 0 auto;
+
+            @media(max-width: ${breakpoints.sm}) {
+                overflow-x: scroll;
+            }
+        }
+    }
+`
+
+const DocImage = ({ name, alt, shadow, width , styles}) => (
+    <StyledDocImage width={width}>
+        <div className="docimage-container">
+            <img className="docimage" src={require(`../resources/${name}`)} alt={alt} style={{boxShadow: shadow ? "0 1.5rem 3rem rgba(0,0,0, .2)" : null, ...styles}}/>
+        </div>
+    </StyledDocImage>
+)
+
+export default DocImage
