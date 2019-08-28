7be22109eb7061c07c2273407bea354ae7b788c2
nisarhassan12 <syednisarhassan12@gmail.com>
2019-08-28 12:47:50 +0000

    Add gatsby-plugin-react-helmet to gatsby-config
    Signed-off-by: nisarhassan12 <syednisarhassan12@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     plugins: [
         'gatsby-plugin-emotion',
+        'gatsby-plugin-react-helmet',
         {
             resolve: 'gatsby-plugin-mdx',
             options: {
