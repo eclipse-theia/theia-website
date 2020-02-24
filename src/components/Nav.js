3676b260dc21f40bd95196f886e37e945e7e9b11
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-02-24 11:12:01 +0000

    [Nav] update the theia link path.
    Signed-off-by: Nisar Hassan Naqvi <syednisarhassan12@gmail.com>

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -187,7 +187,7 @@ class Nav extends React.Component {
                             <a href="https://spectrum.chat/theia" target="_blank" rel="noopener noreferrer" className="nav__link">Community</a>
                         </li>
                         <li className="nav__item">
-                            <a href="https://typefox.io/eclipse-theia" className="nav__link" target="_blank" rel="noopener">Support</a>
+                            <a href="https://www.typefox.io/theia/" className="nav__link" target="_blank" rel="noopener">Support</a>
                         </li>
                         <li className="nav__item">
                             <a href="https://www.typefox.io/trainings/" className="nav__link" target="_blank" rel="noopener">Training</a>
