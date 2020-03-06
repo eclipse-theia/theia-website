da3ed10906c3d2754aa6738126e57d2887062a00
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-03-06 11:06:12 +0000

    refactor + add Toro Cloud to the contributors and adopters list.
    Signed-off-by: Nisar Hassan Naqvi <syednisarhassan12@gmail.com>

---
+++ src/components/ContributorsAndAdopters.js
@@ -0,0 +1,99 @@
+import React from 'react'
+
+import styled from '@emotion/styled'
+import { contributorsAndAdopters } from '../utils/data'
+import { breakpoints } from '../utils/variables'
+
+const Styled = styled.div`
+     .contributors {
+        padding-bottom: 5rem;
+        margin-top: 4rem;
+
+        &__images {
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+
+                @media(min-width: 768px) {
+                    display: grid;
+                    grid-template-columns: repeat(5, 1fr);
+                    grid-row-gap: 80px;
+
+                    & > div {
+                        &:nth-child(n + 6) {
+                            transform: translateX(50%);
+                        }
+                    }
+                }
+
+            @media(max-width: 50rem) {
+                text-align: center;
+            }
+        }
+
+        &__image {
+            height: 100%;
+            max-width: 14rem;
+            object-fit: contain;
+
+            &-container {
+                height: 4rem;
+
+                @media(min-width: 768px) {
+                    height: 4.5rem;
+                    text-align: center;
+                }
+
+                @media(max-width: 50rem) {
+                    width: 40%;
+                    margin-bottom: 4rem;
+                }
+
+                @media(max-width: 30rem) {
+                    width: 100%;
+                }
+            }
+
+            &--ericsson {
+                transform: scale(1.3);
+            }
+
+            &--ibm {
+                transform: scale(.8);
+            }
+
+            &--arm {
+                transform: scale(.5);
+            }
+
+            &--sap {
+                @media(min-width: ${breakpoints.md}) {
+                    transform: translateX(2.5rem);
+                }
+            }
+        }
+    }
+`
+
+const ContributorsAndAdopters = () => (
+    <div className="row">
+        <Styled>
+            <section className="contributors" >
+                <h3 className="heading-tertiary">Contributors & Adopters</h3>
+                <div className="contributors__images">
+                    {
+                        contributorsAndAdopters.map((item, i) => (
+                            <div key={i} className="contributors__image-container">
+                                <a target="_blank" rel="noopener" href={item.href}>
+                                    <img className="contributors__image" src={item.src} alt={item.alt} />
+                                </a>
+                            </div>
+                        ))
+                    }
+                </div>
+            </section>
+        </Styled>
+    </div>
+)
+
+export default ContributorsAndAdopters
---
+++ src/utils/data.js
@@ -0,0 +1,57 @@
+import TypeFoxLogo from '../resources/typefox.png'
+import RedHatLogo from '../resources/redhat.svg'
+import IBMLogo from '../resources/ibm.svg'
+import GoogleLogo from '../resources/google.svg'
+import ARMLogo from '../resources/arm.svg'
+import EricssonLogo from '../resources/ericsson.svg'
+import SAPLogo from '../resources/sap.svg'
+import Arduino from '../resources/arduino.svg'
+import ToroC from '../resources/toro-r.svg'
+
+export const contributorsAndAdopters = [
+    {
+        href: "https://www.typefox.io",
+        src: TypeFoxLogo,
+        alt: "TypeFox Logo"
+    },
+    {
+        href: "https://www.ericsson.com",
+        src: EricssonLogo,
+        alt: "Ericsson Logo"
+    },
+    {
+        href: "https://www.arm.com",
+        src: ARMLogo,
+        alt: "ARM Logo"
+    },
+    {
+        href: "https://www.redhat.com",
+        src: RedHatLogo,
+        alt: "RedHat Logo"
+    },
+    {
+        href: "https://google.com",
+        src: GoogleLogo,
+        alt: "Google Cloud Logo"
+    },
+    {
+        href: "https://www.ibm.com",
+        src: IBMLogo,
+        alt: "IBM Logo"
+    },
+    {
+        href: "https://www.sap.com/",
+        src: SAPLogo,
+        alt: "SAP Logo"
+    },
+    {
+        href: "https://www.arduino.cc/",
+        src: Arduino,
+        alt: "Arduino Logo"
+    },
+    {
+        href: "https://www.torocloud.com/",
+        src: ToroC,
+        alt: "ToroCloud logo"
+    }, 
+]
