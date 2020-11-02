441eac118227ed4920b66fe1619122f8a7659d45
Marc Dumais <marc.dumais@ericsson.com>
2020-11-02 12:30:47 -0500

    [Eclipse Foundation] Re-license the repo
    This repo is to be moved to the Eclipse Theia project. First we
    have to adopt the same license:
    
    EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
    
    This commit adds this license and copyright headers, following
    the re-licensing exercise. See here for the details:
    https://github.com/theia-ide/theia-website/wiki/Re-licensing-theia-ide-theia-website
    
    Note: originally there were no copyright headers in place, in this
    repo. So instead of updating existhing headers to reflect the new
    license, I created new headers, giving credit to the original IP
    rights holder, to the best of my after-the-fact git detective abilities.
    At the end of the day, the git history contains the details of
    where every character in the repo came from, and that's of course the
    source of thruth, not the results of my good faith attempt to figure-out
    who contributed each source code file, here.
    
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,20 @@
-import React from 'react'
+/********************************************************************************
+ * Copyright (C) 2019 TypeFox and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+ import React from 'react'
 
 import Layout from '../layouts/layout'
 import Header from '../components/index/Header'
