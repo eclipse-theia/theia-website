bb451783ca57a591604239e28c6757270f106cd4
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-04-14 15:09:36 -0400

    Add 'remark-autolink-headers` plugin
    The following commit adds the `gatsby-autolink-headers` plugin in order
    to automatically create anchor links in markdown content which can be
    later used to reference individual sections of the documentation.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
+                    'gatsby-remark-autolink-headers',
                     "gatsby-remark-external-links",
                     "gatsby-remark-prismjs"
                 ]
