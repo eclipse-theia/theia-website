733a9971fdfab20c87bf21c43b079779cae6e4b9
Jonas Helming <jhelming@eclipsesource.com>
2021-05-18 07:25:21 +0000

    Rephrased feature section
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>

--- src/components/index/Features.js
+++ src/components/index/Features.js
@@ -21,6 +21,8 @@ import { breakpoints } from '../../utils/variables'
 import IconExtension from '../../resources/icon-extension.svg'
 import IconCloudScreen from '../../resources/icon-cloud-screen.svg'
 import IconOpenSource from '../../resources/icon-open-source.svg'
+import Html from '../../resources/icon-html.svg'
+
 import { Link } from 'gatsby'
 import Feature from './Feature'
 
@@ -42,25 +44,28 @@ const features = [
     {
         img: <img src={IconCloudScreen} alt="Cloud Screen" />,
         title: "Cloud & Desktop",
-        paragraphs: ['Not sure whether you need a web or desktop version or both?', 'With Theia you can develop one IDE and run it in browsers or native desktop application from a single source.']
-    },
-    {
-        img: <img src={IconOpenSource} alt="Vendor Neutral Open Source" />,
-        title: "Vendor Neutral",
-        paragraphs: ['The Theia project is hosted at the Eclipse Foundation, a not-for-profit corporation, and is developed by a diverse community.', <>
-            Unlike other "open-source" projects, projects hosted at an Open-Source Foundation are protected against single-vendor decisions against the interest of the diverse community.
-            Learn more <a href="https://www.eclipse.org/projects/dev_process/">here</a>.
-        </>]
+        paragraphs: ['Not sure whether you need a web or desktop version or both?', 'With Theia you can develop one IDE and run it in a browser or as a native desktop application from a single source.']
     },
     {
         img: <img src={IconExtension} alt="Icon Extension" />,
         title: "Extensible",
-        paragraphs: ['Theia is designed in a modular way to allow extenders and adopters customizing and extending every aspect of it.', 
+        paragraphs: ['Theia is designed in a modular way to allow extenders and adopters to customize and extend every aspect.', 
         <>
-            Composing a custom IDE-like product is as easy as listing all needed extensions in a package.json file. Adding new
-            functionality by implementing <Link to='/docs/authoring_extensions'>
-            your own extensions</Link> is easy, too and provides all the flexibility you need.
+            This goes way beyond “just adding extensions”, because Eclipse Theia  lets you build fully customized or white-labeled products!
         </>]
+    },
+    {
+        img: <img src={IconOpenSource} alt="Vendor Neutral Open Source" />,
+        title: "Vendor Neutral",
+        paragraphs: [ <>
+            Theia is truly vendor-neutral and developed by a diverse community. Unlike other “open source” projects, Theia is actually hosted at an Open-Source Foundation, protecting it against single-vendor decisions that are against the interest of the community.
+            Learn more <a href="https://www.eclipse.org/projects/dev_process/">here</a>.
+        </>]
+    },
+    {
+        img: <img src={Html} alt="Modern Tech" />,
+        title: "Modern Tech Stack",
+        paragraphs: ['Theia is based on a state-of-the-art web-based  technology stack. It provides language support via LSP and DAP. Further, it can host VS Code extensions and provides full terminal access.']
     }
 ]
 
