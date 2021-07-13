38a9b9e848ba302898c393b0f259486198e1bfd8
Max Elia <max_elia@hotmail.de>
2021-07-13 12:16:49 +0200

    Restyling of support page
    Signed-off-by: Max Elia Schweigkofler <max_elia@hotmail.de>

--- src/components/Support.js
+++ src/components/Support.js
@@ -20,10 +20,15 @@ import { breakpoints } from '../utils/variables'
 import styled from '@emotion/styled'
 import EclipseSourceLogo from '../resources/eclipse-source.svg'
 import TypeFoxLogo from '../resources/typefox.png'
+import Background from '../resources/background-image.png'
 
 const StyledSupport = styled.div`
 .header {
     position: relative;
+    background-image: url(${Background});
+    background-size: cover;
+    padding-bottom: 3rem;
+    border-bottom: 10px solid #f8f8f8;
     @media(max-width: ${breakpoints.xmd}) {
         padding-top: 15rem;
     }
@@ -58,54 +63,49 @@ const StyledSupport = styled.div`
         
     }
 }
-    h1 {
+h1 {
+    margin-bottom: 1rem;
+    font-size: 3.4rem;
+}
+.support {
+    padding: 3rem 0;
+}
+
+.support__container {
+    display: flex;
+
+    @media(max-width: ${breakpoints.md}) {
+        flex-direction: column;
+    }
+}
+.reverse {
+    flex-direction: row-reverse;
+    @media(max-width: ${breakpoints.md}) {
+        flex-direction: column-reverse;
+    }
+}
+.supporter {
+    flex: 1;
+    margin: 2rem 0;
+
+    & > div {
+        text-align: center;
+    }
+
+    h3 {
+        margin-top: 2rem;
+    }
+
+    & > p {
+        margin: 0 auto;
         margin-bottom: 1rem;
-        font-size: 3.4rem;
-    }
-    .support {
-        padding: 1rem 0;
+        max-width: 45rem;
     }
 
-    .support__container {
-        display: flex;
-
-        @media(max-width: ${breakpoints.md}) {
-            flex-direction: column;
-        }
+    img {
+        height: 7rem;
     }
-    .reverse {
-        flex-direction: row-reverse;
-        @media(max-width: ${breakpoints.md}) {
-            flex-direction: column-reverse;
-        }
-    }
-    .supporter {
-        flex: 1;
-
-        &:not(:last-child) {
-            @media(max-width: ${breakpoints.md}) {
-                margin: 0 0 4rem 0;
-            }
-        }
-
-        & > div {
-            text-align: center;
-        }
-
-        h3 {
-            margin-top: 2rem;
-        }
-
-        & > p {
-            margin: 0 auto;
-            margin-bottom: 1rem;
-            max-width: 45rem;
-        }
-
-        img {
-            height: 7rem;
-        }
-    }
+}
 `
 
 const supporters = [
@@ -130,19 +130,21 @@ const Support = () => (
         <header className='header' role="banner">
             <div className="row">
                 <Nav shouldRenderLogo={true} />
+                <div className="header__text-box">
+                    <h1 className="heading-primary">
+                    Professional Support
+                    </h1>
+                    <h2 style={{ fontSize: '2.2rem' }}>
+                    The following contributing companies provide professional support and training for Eclipse Theia:
+                    </h2>
+                </div>
             </div>
         </header>
         <section className="support row" id="support">
-            <h1 className="heading-primary">
-                Professional Support
-            </h1>
-            <p>
-                The following contributing companies provide professional support and training for Eclipse Theia:
-            </p>
-            <div className={`support__container${Math.random() < 0.5 ? ' reverse' : ''}`}>
+            <div className={`support__container ${Math.random() < 0.5 && 'reverse'}`}>
                 {
                     supporters.map(
-                        ({title ,link, img, training, support},i) => {
+                        ({title, link, img, training, support},i) => {
                             return(
                             <div key={`${i}_${title}`} className="supporter">
                                 <div>
