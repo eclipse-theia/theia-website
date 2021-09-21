690b75b4cf945dd91cff22514641afc5ba45f696
dante-di-domenico <72292738+Archie27376@users.noreply.github.com>
2021-09-21 09:43:32 -0400

    Fixed syntax to be `'15px'`

--- src/components/index/Header.js
+++ src/components/index/Header.js
@@ -88,7 +88,7 @@ const Header = () => (
                     <iframe title="Github Fork Count" className="header__github-button" src="https://ghbtns.com/github-btn.html?user=eclipse-theia&repo=theia&type=fork&count=true" frameBorder={0} scrolling={0} />
                 </div>
                 <div className="header__text-box">
-                    <div className="header_banner" style={{ padding:15px; text-align:center; }}>
+                    <div className="header_banner" style={{ padding: '15px'; text-align: center; }}>
                         <div className="banner">
                             <img className="banner_image" src="https://lh5.googleusercontent.com/rYzgav3AjXGZctms…cU0cOvwrlQsZdmbwH7fJA2M0VywmydZPTFFfsLm9XAQ=w1600" alt="image_1" style={{ width:100% }}></img>
                             <h1 style={{ fontSize: '2.2rem' }}>
