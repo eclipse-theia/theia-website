a3af669869e3016c93088a0cc6ed56cc47c6ecc8
Chris Radke <c.radke@posteo.de>
2022-05-02 11:57:07 +0200

    Add VUEngine Studio screenshot to "Selected Tools based on Eclipse Theia" (#287)
    Signed-off-by: Christian Radke <c.radke@posteo.de>

--- src/utils/data.js
+++ src/utils/data.js
@@ -14,26 +14,27 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
- import GoogleLogo from '../resources/google.svg'
- import ToroC from '../resources/toro-r.svg'
- import Huawei from '../resources/huawei.svg'
- import RecordEvolutionLogo from '../resources/record-evolution-logo.svg' 
- import RecordEvolutionScreenshot from '../resources/screenshots/record-evolution-screenshot.png'
- import RecordEvolutionScreenshotMin from '../resources/screenshots/thumbnails/record-evolution-screenshot-min.png'
- import MbedStudio from '../resources/screenshots/arm_mbedstudio-ss.png'
- import MbedStudioMin from '../resources/screenshots/thumbnails/arm_mbedstudio-ss-min.png'
- import CodeReady from '../resources/screenshots/redhat-crw-ss.png'
- import CodeReadyMin from '../resources/screenshots/thumbnails/redhat-crw-ss-min.png'
- import CoffeeEditor from '../resources/screenshots/coffeeditor.gif'
- import CoffeeEditorMin from '../resources/screenshots/thumbnails/coffeeditor-min.gif'
- import LogiCloud from '../resources/screenshots/logicloud.png'
- import LogiCloudMin from '../resources/screenshots/thumbnails/logicloud-min.png'
- import BluePrint from '../resources/screenshots/blueprint.png'
- import BluePrintMin from '../resources/screenshots/thumbnails/blueprint-min.png'
- import JonasHelming from '../resources/intros/jonas-helming.jpg'
- import MarcDumais from '../resources/intros/marc-dumais.jpg'
- import ThomasMader from '../resources/intros/thomas-mader.jpg'
- 
+import GoogleLogo from '../resources/google.svg'
+import ToroC from '../resources/toro-r.svg'
+import Huawei from '../resources/huawei.svg'
+import VUEngineStudioScreenshot from '../resources/screenshots/vuengine-studio.png'
+import VUEngineStudioScreenshotMin from '../resources/screenshots/thumbnails/vuengine-studio-min.png'
+import RecordEvolutionScreenshot from '../resources/screenshots/record-evolution-screenshot.png'
+import RecordEvolutionScreenshotMin from '../resources/screenshots/thumbnails/record-evolution-screenshot-min.png'
+import MbedStudio from '../resources/screenshots/arm_mbedstudio-ss.png'
+import MbedStudioMin from '../resources/screenshots/thumbnails/arm_mbedstudio-ss-min.png'
+import CodeReady from '../resources/screenshots/redhat-crw-ss.png'
+import CodeReadyMin from '../resources/screenshots/thumbnails/redhat-crw-ss-min.png'
+import CoffeeEditor from '../resources/screenshots/coffeeditor.gif'
+import CoffeeEditorMin from '../resources/screenshots/thumbnails/coffeeditor-min.gif'
+import LogiCloud from '../resources/screenshots/logicloud.png'
+import LogiCloudMin from '../resources/screenshots/thumbnails/logicloud-min.png'
+import BluePrint from '../resources/screenshots/blueprint.png'
+import BluePrintMin from '../resources/screenshots/thumbnails/blueprint-min.png'
+import JonasHelming from '../resources/intros/jonas-helming.jpg'
+import MarcDumais from '../resources/intros/marc-dumais.jpg'
+import ThomasMader from '../resources/intros/thomas-mader.jpg'
+
 export const contributorsAndAdopters = [
     {
         homepage_url: "https://about.google/",
@@ -52,28 +53,28 @@ export const contributorsAndAdopters = [
     }
 ]
 
- export const introsToTheia = [
-     {
-         title: "Why Eclipse Theia",
-         href: "https://youtu.be/xs0haWTulrY",
-         speaker: "Marc Dumais, Theia project lead and Software Developer at Ericsson AB",
-         src: MarcDumais,
-     },
-     {
-         title: "How to be an Eclipse Theia Adopter",
-         href: "https://youtu.be/SPO8pudgJak",
-         speaker: "Jonas Helming, Theia project lead and Principal Software Architect at EclipseSource",
-         src: JonasHelming,
-     },
-     {
-         title: "The Eclipse Theia Architecture",
-         href: "https://youtu.be/KN2JUsFuEhU",
-         speaker: "Thomas Mäder, Theia project lead and Principal Software Developer at Red Hat",
-         src: ThomasMader,
-     }
- ]
- 
- export const products = [
+export const introsToTheia = [
+    {
+        title: "Why Eclipse Theia",
+        href: "https://youtu.be/xs0haWTulrY",
+        speaker: "Marc Dumais, Theia project lead and Software Developer at Ericsson AB",
+        src: MarcDumais,
+    },
+    {
+        title: "How to be an Eclipse Theia Adopter",
+        href: "https://youtu.be/SPO8pudgJak",
+        speaker: "Jonas Helming, Theia project lead and Principal Software Architect at EclipseSource",
+        src: JonasHelming,
+    },
+    {
+        title: "The Eclipse Theia Architecture",
+        href: "https://youtu.be/KN2JUsFuEhU",
+        speaker: "Thomas Mäder, Theia project lead and Principal Software Developer at Red Hat",
+        src: ThomasMader,
+    }
+]
+
+export const products = [
     {
         href: "https://os.mbed.com/studio/",
         src: MbedStudio,
@@ -109,6 +110,11 @@ export const contributorsAndAdopters = [
         src: RecordEvolutionScreenshot,
         thumb: RecordEvolutionScreenshotMin,
         alt: "Record Evolution Screenshot"
+    },
+    {
+        href: "https://www.vuengine.dev",
+        src: VUEngineStudioScreenshot,
+        thumb: VUEngineStudioScreenshotMin,
+        alt: "VUEngine Studio"
     }
- ]
- 
+]
