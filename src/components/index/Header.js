0b9d053986c950dc6e12e8e6dd96025bf60ac16d
Jonas Helming <jhelming@eclipsesource.com>
2022-09-09 10:00:50 +0000

    Add links to new release articles
    fixed #322

--- src/components/index/Header.js
+++ src/components/index/Header.js
@@ -98,7 +98,7 @@ const Header = () => (
                     <h2 style={{ fontSize: '2.1rem' }}>
                         Efficiently develop and deliver Cloud & Desktop IDEs and tools with modern web technologies.
                         <br/>
-                        <a href="https://eclipsesource.com/blogs/2022/07/11/eclipse-theia-1-27-release-news-and-noteworthy/" rel="noopener noreferrer">Learn about the latest 1.27 Release!</a>
+                        <a href="https://eclipsesource.com/blogs/2022/09/09/eclipse-theia-1-29-release-news-and-noteworthy/" rel="noopener noreferrer">Learn about the latest 1.29 Release!</a>
                         <br/>
                         Stay up-to-date: <a href="https://twitter.com/theia_ide">follow us on Twitter</a> and <a href="https://accounts.eclipse.org/mailing-list/friends-of-theia">register to the "Friends of Theia" mailing list</a>.
                     </h2>
