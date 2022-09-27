609c3c306a3d7e82a617659476ed5955181a4bbb
Jonas Helming <jhelming@eclipsesource.com>
2022-09-27 15:11:26 +0200

    adapt community release plan (#329)
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>

--- src/components/Releases.js
+++ src/components/Releases.js
@@ -73,6 +73,8 @@ const communityReleases = [
         name: 'Theia 1.29.x (September 2022) - planned',
         releasedate: 'September 29th, 2022',
         releasecandidatedate:'August 25th, 2022',
+        technologiesin:'October 6th, 2022',
+        releaseanouncement:'October 13, 2022',
         frameworks: [
             {
                 title: 'Eclipse GLSP',
@@ -106,12 +108,14 @@ const Releases = () => (
                 </div>
             </div>
         </header>
-        {communityReleases.map(({ name, frameworks, releasedate, releasecandidatedate }, i) => {
+        {communityReleases.map(({ name, frameworks, releasedate, releasecandidatedate, technologiesin,releaseanouncement }, i) => {
             return (
                 <section key={i} className="category row" id="frameworks">
                     <h2 className="heading-secondary">{name}</h2>
-                    <p>Release Date: {releasedate}</p>
-                    <p>Community Release Candidate Date: {releasecandidatedate}</p>
+                    <p>Community Release Candidate: <b>{releasecandidatedate}</b></p>
+                    <p>Community Release Available: <b>{releasedate}</b></p>
+                    <p>Compatible Technologies Listed: <b>{technologiesin}</b></p>
+                    <p>Community Release Announcement: <b>{releaseanouncement}</b></p>
                     <br></br>
                     <p><h3 className="heading-tertiary"><b>Compatible Technologies:</b></h3></p>
                     {frameworks.map(
