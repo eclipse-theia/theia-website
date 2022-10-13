b70e3af754422d2f5e963be7da02294e805b3812
Jonas Helming <jhelming@eclipsesource.com>
2022-10-13 09:47:17 +0000

    Add links to Theia release 1.30
    fixed #347
    
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -80,6 +80,11 @@ const categories = [
         title: 'Release announcements',
         resources: [
             {
+                title: 'Eclipse Theia 1.30 Release: News and Noteworthy',
+                url: 'https://eclipsesource.com/blogs/2022/10/13/eclipse-theia-1-30-release-news-and-noteworthy/',
+                type: 'article'
+            },
+            {
                 title: 'Eclipse Theia 1.29 Release: News and Noteworthy',
                 url: 'https://eclipsesource.com/blogs/2022/09/09/eclipse-theia-1-29-release-news-and-noteworthy/',
                 type: 'article'
