1ff1592312638eaa4a0f05a4002c2d22c94900fc
Jonas Helming <jhelming@eclipsesource.com>
2023-12-14 15:56:30 +0100

    Add community release 2023-11 (#490)
    * Add community release 2023-11
    
    fixed #489
    
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>
    
    * Update src/components/Releases.js
    
    Co-authored-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>
    
    ---------
    
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>
    Co-authored-by: Johannes Faltermeier <jfaltermeier@eclipsesource.com>

--- src/components/Releases.js
+++ src/components/Releases.js
@@ -73,12 +73,59 @@ heading-primary {
 
 const communityReleases = [
     {
-        name: 'Theia 1.43.x (2023-11) - planned',
+        name: 'Theia 1.45.x (2023-02) - planned',
+        releasedate: 'January 25th, 2024',
+        releasecandidatedate: 'December 21th, 2023',
+        technologiesin: 'February 1st, 2024',
+        releaseanouncement: 'February 8th, 2024',
+        announcementurl: '',
+        frameworks: [
+            {
+                title: 'Eclipse GLSP',
+                url: 'https://www.eclipse.dev/glsp/',
+                version: 'TBD',
+                modules: []
+            },
+            {
+                title: 'Eclipse EMF.cloud',
+                url: 'https://www.eclipse.dev/emfcloud/',
+                version: 'TBD',
+                modules: []
+            },
+            {
+                title: 'Eclipse Sprotty',
+                url: 'https://www.eclipse.dev/sprotty/',
+                version: 'TBD',
+                modules: []
+            },
+            {
+                title: 'Eclipse CDT Cloud Debug Adapter',
+                url: 'https://projects.eclipse.org/projects/ecd.cdt-cloud',
+                version: 'TBD',
+                modules: []
+            },
+            {
+                title: 'Langium',
+                url: 'https://langium.org/',
+                version: 'TBD',
+                modules: []
+            },
+            {
+                title: 'Theia Trace Extension',
+                url: 'https://github.com/eclipse-cdt-cloud/theia-trace-extension',
+                version: 'TBD',
+                modules: []
+            }
+        ]
+    },
+
+    {
+        name: 'Theia 1.43.x (2023-11) - published',
         releasedate: 'November 30th, 2023',
         releasecandidatedate: 'October 26th, 2023',
         technologiesin: 'December 7th, 2023',
         releaseanouncement: 'December 14th, 2023',
-        announcementurl: '',
+        announcementurl: 'https://eclipsesource.com/blogs/2023/12/14/the-eclipse-theia-community-release-2023-11/',
         frameworks: [
             {
                 title: 'Eclipse GLSP',
