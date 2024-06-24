a4208f31ddef0a4424c1e3ae6a00bb4dbd787cb6
Niklas Dahlquist <niklas.dahlquist@st.com>
2024-06-24 08:47:28 +0200

    fix: Added ref to 1.50 release in header (#566)
    Contributed by STMicroelectronics
    
    Signed-off-by: Niklas DAHLQUIST <niklas.dahlquist@st.com>

--- src/components/index/Header.js
+++ src/components/index/Header.js
@@ -106,7 +106,7 @@ const Header = () => (
                 </div>
                 <div className="header__banner">
                     <h2 style={{ fontSize: '2.1rem' }}>
-                        <a href="https://eclipsesource.com/blogs/2024/05/08/eclipse-theia-1-49-release-news-and-noteworthy/" rel="noopener noreferrer">Learn about the latest 1.49 Release</a> and <a href="https://eclipsesource.com/blogs/2024/06/20/the-eclipse-theia-community-release-2024-05/">the latest Community Release (2024-05)</a>.
+                        <a href="https://eclipsesource.com/blogs/2024/06/21/eclipse-theia-1-50-release-news-and-noteworthy/" rel="noopener noreferrer">Learn about the latest 1.50 Release</a> and <a href="https://eclipsesource.com/blogs/2024/06/20/the-eclipse-theia-community-release-2024-05/">the latest Community Release (2024-05)</a>.
                         <br/>
                         Stay up-to-date: <a href="https://twitter.com/theia_ide">follow us on Twitter</a> and <a href="https://accounts.eclipse.org/mailing-list/friends-of-theia">register to the "Friends of Theia" mailing list</a>.
                     </h2>
