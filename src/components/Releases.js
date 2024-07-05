25a89c7a8d706dba7aa3a8f676d9607b5c126972
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2024-07-05 02:43:49 -0400

    theia-trace-extension update for community release 1.49.x (#578)
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- src/components/Releases.js
+++ src/components/Releases.js
@@ -166,8 +166,12 @@ const communityReleases = [
             {
                 title: 'Theia Trace Extension',
                 url: 'https://github.com/eclipse-cdt-cloud/theia-trace-extension',
-                version: 'TBD',
-                modules: []
+                version: '0.2.4',
+                modules: [
+                    { modulename: 'theia-traceviewer', url: 'https://www.npmjs.com/package/theia-traceviewer' },
+                    { modulename: 'traceviewer-base', url: 'https://www.npmjs.com/package/traceviewer-base' },
+                    { modulename: 'traceviewer-react-components', url: 'https://www.npmjs.com/package/traceviewer-react-components' }
+                ]
             }
         ]
     },
