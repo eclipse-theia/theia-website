2987e1a2e2ac11f7b80d7f35bae6ff8defb01394
Jonas Helming <jhelming@eclipsesource.com>
2024-10-30 15:03:21 +0100

    Add Snap Download as preferred option (#636)
    fixed #635
    
    Signed-off-by: Jonas Helming <jhelming@eclipsesource.com>

--- src/components/index/TheiaIDEDownloads.js
+++ src/components/index/TheiaIDEDownloads.js
@@ -46,19 +46,6 @@ const StyledHeader = styled.div`
             margin-bottom: 1rem;
             font-size: 2.2rem;
         }
-        .btn {
-            max-width: 21rem;
-            @media(max-width: 385px) {
-                &:not(:last-child) {
-                    margin-right: 0;
-                }
-            }
-            @media(max-width: 800px) {
-                &:not(:last-child) {
-                    margin-right: 2rem;
-                }
-            }
-        }
         &__github-details {
             position: absolute;
             top: 10rem;
@@ -70,9 +57,18 @@ const StyledHeader = styled.div`
             align-items: center;
             text-align: center;
             padding: 3rem 10vw;
-            @media(max-width: 800px){
+            @media(max-width: 600px){
+                flex-direction: column;
                 padding: 3rem 0;
             }
+            .btn {
+            max-width: 21rem;
+            @media(max-width: 800px) {
+                &:not(:last-child) {
+                    margin-bottom: 2rem
+                }
+            }
+        }
             
         }
         iframe {
@@ -92,13 +88,12 @@ const TheiaIDEDownloads = () => (
                 We are continuing to make improvements and add functionality to the Theia IDE, we welcome and value your feedback (see below).
                 
                 <div className="header__buttons">
-                    <a className="btn btn--cta" href="https://www.eclipse.org/downloads/download.php?file=/theia/ide/latest/windows/TheiaIDESetup.exe&r=1" rel="noopener">Windows (latest)</a>
-                    <a className="btn btn--cta" href="https://www.eclipse.org/downloads/download.php?file=/theia/ide/latest/linux/TheiaIDE.AppImage&r=1" rel="noopener">Linux (latest)</a>
-                    <a className="btn btn--cta" href="https://www.eclipse.org/downloads/download.php?file=/theia/ide/latest/macos/TheiaIDE.dmg&r=1" rel="noopener">MacOS (latest)</a>
+                    <a className="btn btn--cta" href="https://www.eclipse.org/downloads/download.php?file=/theia/ide/latest/windows/TheiaIDESetup.exe&r=1" rel="noopener">Windows</a>
+                    <a className="btn btn--cta" href="https://www.eclipse.org/downloads/download.php?file=/theia/ide/latest/macos/TheiaIDE.dmg&r=1" rel="noopener">MacOS</a>
+                    <a className="btn btn--cta" href="https://snapcraft.io/theia-ide" rel="noopener">Linux (Snap)</a>
+                    <a className="btn btn--cta" href="https://www.eclipse.org/downloads/download.php?file=/theia/ide/latest/linux/TheiaIDE.AppImage&r=1" rel="noopener">Linux (AppImage)</a>
                  </div>
-                
                  <center>
-                <a href="https://snapcraft.io/theia-ide">Get the Theia IDE from the Snap Store</a><br></br>
                  You can also <a href="https://try.theia-cloud.io/">try the latest version of the Theia IDE online</a>. The online test version is limited to 30 minutes per session and hosted via <a href="https://github.com/eclipsesource/theia-cloud">Theia.cloud</a>.<br></br>
                  Finally, we provide an <a href="https://github.com/eclipse-theia/theia-blueprint?tab=readme-ov-file#docker">experimental Docker image</a> for hosting the Theia IDE online.
                  </center>
