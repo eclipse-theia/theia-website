60e2927e7a2552611afa7c763c63e21e0df1d2e3
Jonas Helming <jhelming@eclipsesource.com>
2024-12-18 12:09:57 +0100

    Add theiaCon videos (#685)
    fixed #684

--- src/components/index/CommonNews.js
+++ src/components/index/CommonNews.js
@@ -19,6 +19,8 @@ const PlatformHeader = () => (
             <a href="https://eclipsesource.com/blogs/2024/12/06/eclipse-theia-1-56-release-news-and-noteworthy/" rel="noopener noreferrer">Learn about the latest 1.56 Release</a> and <a href="https://eclipsesource.com/blogs/2024/12/12/the-eclipse-theia-community-release-2024-11/">the latest Community Release (2024-11)</a>.
             <br />
             Stay up-to-date: <a href="https://twitter.com/theia_ide">follow us on Twitter</a> and <a href="https://accounts.eclipse.org/mailing-list/friends-of-theia">register to the "Friends of Theia" mailing list</a>.
+            <br />
+            In case you missed TheiaCon 2024, all talk recordings are available now, see <a href="https://www.youtube.com/playlist?list=PLy7t4z5SYNaQAGs0tr_ZSv3h2GKH2G50X">here</a>!
         </h2>
     </div>
 )
