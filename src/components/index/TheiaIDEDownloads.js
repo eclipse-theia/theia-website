8d9d5a85ac33876e5821ab0481af51412d9a35a4
Thomas Mäder <tsmaeder@users.noreply.github.com>
2025-03-05 11:22:26 +0100

    Apply suggestions from code review

--- src/components/index/TheiaIDEDownloads.js
+++ src/components/index/TheiaIDEDownloads.js
@@ -94,12 +94,12 @@ const TheiaIDEDownloads = () => (
                     <a className="btn btn--cta" href="https://www.eclipse.org/downloads/download.php?file=/theia/ide/latest/macos/TheiaIDE.dmg&r=1" rel="noopener">MacOS (x86)¹</a>
                 </div>
                  <div className="header__buttons">
-                    <a className="btn btn--cta" href="insert link" rel="noopener">MacOS (ARM, <i>experimental</i>)²</a>
+                    <a className="btn btn--cta" href="https://github.com/eclipse-theia/theia-ide/releases/download/pre-release/TheiaIDE-arm64-patched.dmg.zip" rel="noopener">MacOS (ARM, <i>experimental</i>)²</a>
                  </div>
                  <center>
                  <i>¹ The Mac x86 version might run slow and unstable on recent ARM systems. Please try our new dedicated ARM build.</i>
                  <br></br>
-                 <i>² The Mac ARM version is new, experimental and currenlty still unsigned (see <a href="https://support.apple.com/guide/mac-help/open-a-mac-app-from-an-unknown-developer-mh40616/mac">here</a> how to start it). We are working on providing a signed version asap.</i>
+<i>² The Mac ARM version is new, experimental and currently still unsigned. Newer versions of MacOS set the `quarantine` attribute on programs downloaded from the internet and refuse to run the application. I order to still run the build, you will have to clear the `quarantine` attribute from the application. In a terminal, type 
                  <br></br>
                  You can also <a href="https://try.theia-cloud.io/">try the latest version of the Theia IDE online</a>. The online test version is limited to 30 minutes per session and hosted via <a href="https://github.com/eclipsesource/theia-cloud">Theia.cloud</a>.<br></br>
                  Finally, we provide an <a href="https://github.com/eclipse-theia/theia-blueprint?tab=readme-ov-file#docker">experimental Docker image</a> for hosting the Theia IDE online.
